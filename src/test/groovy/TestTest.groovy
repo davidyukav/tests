3c30e2d49c024574f86c059d3749baff0fc800ff
davidyuk <davidyuk.av@mail.ru>
2017-01-29 13:25:40 +1000

    blablatest

--- src/test/groovy/TestTest.groovy
+++ src/test/groovy/TestTest.groovy
@@ -1,13 +1,16 @@
 import org.jsoup.Jsoup
+import org.openqa.selenium.firefox.FirefoxDriver
 import org.testng.annotations.Test
 
 class TestTest {
 
     @Test()
     void BlablaTest() {
-        def result = Jsoup.connect("http://www.domotekhnika.ru  ")
+        def result = Jsoup.connect("http://www.domotekhnika.ru")
         println result.class
         println result
+        def browser = new FirefoxDriver()
+        browser.get("http://www.domotekhnika.ru")
     }
 
 }
