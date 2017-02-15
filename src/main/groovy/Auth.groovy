08278ac72eb3f78945d383d3404db4d95b31002f
devidyukav <1Qazxcvb>
2017-02-15 18:10:14 +1000

    readme

--- src/main/groovy/Auth.groovy
+++ src/main/groovy/Auth.groovy
@@ -1,9 +1,6 @@
 import com.codeborne.selenide.Condition
 import org.openqa.selenium.By
-import org.testng.annotations.Test
-
 import static com.codeborne.selenide.Selenide.$
-import static com.codeborne.selenide.Selenide.open
 
 class Auth {
 
@@ -20,7 +17,7 @@ class Auth {
         $(By.linkText("Регистрация")).click()
         $(By.name("fio")).setValue(fio)
         $(By.name("phone")).setValue(phone)
-        $(By.cssSelector(".js-auth__register-form .b-form__text.i-show-pass")).setValue(pass)
+        $(".js-auth__register-form .b-form__text.i-show-pass").setValue(pass)
         $(By.name("email")).setValue(email)
         $(By.name("terms")).parent().click()
         $(".js-tm-registration_btn").click()
